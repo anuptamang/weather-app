21e6af4436ac51c913857fbdc15fc5b4dde44769
anuptamang <anupkumartamang@gmail.com>
2019-07-09 23:31:18 +0545

    first commit to heroku

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('.form-control')
+const weatherDetails = document.querySelector('.weather-details')
+
+const hostname = location.origin;
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value
+
+    fetch(`${hostname}/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                weatherDetails.innerHTML = data.error
+                weatherDetails.classList.remove('d-none')
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+
+                let output = `
+                    <h4 class="mb-3 text-white">${data.location}</h4>
+                    <p>${data.forecast}</p>
+                `;
+
+                weatherDetails.innerHTML = output
+                weatherDetails.classList.remove('d-none')
+
+            }
+
+        })
+    })
+})
--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -19,7 +21,7 @@ app.use(express.static(publicDir));
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather Info',
+        title: 'Get Weather Info',
         name: 'Anup Tamang'
     })
 })
@@ -39,13 +41,37 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-    res.send({
-        forecast: 'It is snowing',
-        location: 'philadelphia'
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide a address'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
 })
 
 app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a serch term'
+        })
+    }
     res.send({
         products: []
     })
